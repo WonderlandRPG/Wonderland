68a9f90e05c015747a26575d76f058277908ab2d
WonderlandRPG <rpgwonderlandoficial@gmail.com>
2026-08-04 20:57:55 -0300

    Corrige URL do projeto Supabase

--- js/supabase-client.js
+++ js/supabase-client.js
@@ -1,7 +1,7 @@
 "use strict";
 
 (function(){
-  const SUPABASE_URL="https://wvgwcvklbkyvgpxxkfot.supabase.co";
+  const SUPABASE_URL="https://vwgwcvklbkyvgpxxkfot.supabase.co";
   const SUPABASE_PUBLISHABLE_KEY="sb_publishable_M01E7oHWDI5hH-v8THFKHg_3h4_atRN";
 
   if(!window.supabase?.createClient){

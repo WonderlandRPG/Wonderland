401eaab3080cf47f0e015d06d5b9c260da0e4878
WonderlandRPG <rpgwonderlandoficial@gmail.com>
2026-08-06 03:50:48 -0300

    Prepara multiplicadores antes de iniciar a Arena

--- js/pages/arena-bootstrap.js
+++ js/pages/arena-bootstrap.js
@@ -4,6 +4,9 @@ try {
   if (window.WONDERLAND_CONTENT_READY) {
     await window.WONDERLAND_CONTENT_READY;
   }
+  if (window.WONDERLAND_PREPARE_ARENA_SCALING) {
+    await window.WONDERLAND_PREPARE_ARENA_SCALING();
+  }
 } catch (error) {
   console.warn("A Arena seguirá com o catálogo local normalizado.", error);
 }

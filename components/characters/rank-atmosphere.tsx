c7e9045da003efe4daea039e0658a3b6d0877ad7
WonderlandRPG <rpgwonderlandoficial@gmail.com>
2026-08-16 17:34:05 -0300

    Remove rank atmosphere visuals

--- components/characters/rank-atmosphere.tsx
+++ components/characters/rank-atmosphere.tsx
@@ -2,17 +2,6 @@ interface RankAtmosphereProps {
   rank?: string;
 }
 
-export function RankAtmosphere({ rank }: RankAtmosphereProps) {
-  if (!rank) return null;
-
-  return (
-    <div className="rank-atmosphere" data-rank-atmosphere={rank} aria-hidden="true">
-      <span className="rank-atmosphere__veil" />
-      {Array.from({ length: 24 }, (_, index) => (
-        <i key={index} style={{ "--particle-index": index } as React.CSSProperties}>
-          <span />
-        </i>
-      ))}
-    </div>
-  );
+export function RankAtmosphere({ rank: _rank }: RankAtmosphereProps) {
+  return null;
 }

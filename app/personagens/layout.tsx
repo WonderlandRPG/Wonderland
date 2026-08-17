96791184e0e76bc54e262936635d523a487e43ab
WonderlandRPG <rpgwonderlandoficial@gmail.com>
2026-08-17 11:03:30 -0300

    style: load portrait upload controls

--- app/personagens/layout.tsx
+++ app/personagens/layout.tsx
@@ -1,5 +1,6 @@
 import "./personagens-route.css";
 import "./equipment-layout.css";
+import "./character-image-upload.css";
 
 export default function CharactersLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return children;

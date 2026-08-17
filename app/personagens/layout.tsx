f84eaf31e46776d95d9dd9a084fb9d502b197a0c
WonderlandRPG <rpgwonderlandoficial@gmail.com>
2026-08-17 11:53:33 -0300

    fix: load equipment modal styles

--- app/personagens/layout.tsx
+++ app/personagens/layout.tsx
@@ -1,5 +1,6 @@
 import "./personagens-route.css";
 import "./equipment-layout.css";
+import "./equipment-modal.css";
 import "./character-image-upload.css";
 
 export default function CharactersLayout({ children }: Readonly<{ children: React.ReactNode }>) {

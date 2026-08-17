6a977617e89e011ebccfdb37cece2c5de92380d6
WonderlandRPG <rpgwonderlandoficial@gmail.com>
2026-08-17 09:23:58 -0300

    style: load the isolated equipment layout repair

--- app/personagens/layout.tsx
+++ app/personagens/layout.tsx
@@ -1,4 +1,5 @@
 import "./personagens-route.css";
+import "./equipment-layout.css";
 
 export default function CharactersLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return children;

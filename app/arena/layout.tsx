f97f18f580b4123eaff91e038933cf5acec47178
WonderlandRPG <rpgwonderlandoficial@gmail.com>
2026-08-17 13:53:34 -0300

    fix: activate arena image repair layer

--- app/arena/layout.tsx
+++ app/arena/layout.tsx
@@ -1,5 +1,13 @@
+import { ArenaImageRepair } from "@/components/arena/arena-image-repair";
+
 import "./arena.css";
+import "./arena-images.css";
 
 export default function ArenaLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return children;
+  return (
+    <>
+      <ArenaImageRepair />
+      {children}
+    </>
+  );
 }

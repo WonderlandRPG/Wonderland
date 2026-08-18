c0c826c13736a260f6340e7c54a52b5c87298887
WonderlandRPG <rpgwonderlandoficial@gmail.com>
2026-08-18 08:42:37 -0300

    load combat polish styles

--- app/arena/layout.tsx
+++ app/arena/layout.tsx
@@ -3,6 +3,7 @@ import { ArenaImageRepair } from "@/components/arena/arena-image-repair";
 import "./arena.css";
 import "./arena-images.css";
 import "./jrpg.css";
+import "./combat-polish.css";
 
 export default function ArenaLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (

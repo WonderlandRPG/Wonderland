ae87a171018a9bdc9f4561203f39e3e82ba64bef
WonderlandRPG <rpgwonderlandoficial@gmail.com>
2026-08-18 18:44:10 -0300

    add arena entry recovery layer

--- app/arena/layout.tsx
+++ app/arena/layout.tsx
@@ -1,3 +1,4 @@
+import { ArenaEntryNavigation } from "@/components/arena/arena-entry-navigation";
 import { ArenaImageRepair } from "@/components/arena/arena-image-repair";
 import { BattleFormationEnhancer } from "@/components/arena/battle-formation-enhancer";
 
@@ -7,10 +8,12 @@ import "./jrpg.css";
 import "./combat-polish.css";
 import "./jrpg-formation.css";
 import "./arena-depth.css";
+import "./entry-hotfix.css";
 
 export default function ArenaLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <>
+      <ArenaEntryNavigation />
       <ArenaImageRepair />
       <BattleFormationEnhancer />
       {children}

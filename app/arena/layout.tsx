57aa002c6c7f8302d4a150a446bd57b49d9dfaa1
WonderlandRPG <rpgwonderlandoficial@gmail.com>
2026-08-18 07:55:58 -0300

    load JRPG arena styles

--- app/arena/layout.tsx
+++ app/arena/layout.tsx
@@ -2,6 +2,7 @@ import { ArenaImageRepair } from "@/components/arena/arena-image-repair";
 
 import "./arena.css";
 import "./arena-images.css";
+import "./jrpg.css";
 
 export default function ArenaLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (

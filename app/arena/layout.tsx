c207fb54e3c2629a2f5fe6ce0dcf65ca0868f210
WonderlandRPG <rpgwonderlandoficial@gmail.com>
2026-08-19 19:39:06 -0300

    load pvp party styles

--- app/arena/layout.tsx
+++ app/arena/layout.tsx
@@ -12,6 +12,7 @@ import "./arena-depth.css";
 import "./entry-hotfix.css";
 import "./combat-immersion.css";
 import "./combat-clarity.css";
+import "./pvp-party.css";
 
 export default function ArenaLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (

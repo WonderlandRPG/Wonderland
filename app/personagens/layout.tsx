542b6dff12f35a67def8875d78a91928378bfb9f
WonderlandRPG <rpgwonderlandoficial@gmail.com>
2026-08-25 02:16:08 -0300

    feat(ui): aprimorar ficha, inventário e experiência visual

--- app/personagens/layout.tsx
+++ app/personagens/layout.tsx
@@ -2,6 +2,7 @@ import "./personagens-route.css";
 import "./equipment-layout.css";
 import "./equipment-modal.css";
 import "./character-image-upload.css";
+import "./sheet-experience.css";
 
 export default function CharactersLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return children;

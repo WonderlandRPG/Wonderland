21293a2b926b70630a78cb518b4129ac8a14e69e
WonderlandRPG <rpgwonderlandoficial@gmail.com>
2026-08-29 01:51:41 -0300

    feat: load character cosmetic styles

--- app/personagens/layout.tsx
+++ app/personagens/layout.tsx
@@ -3,6 +3,7 @@ import "./equipment-layout.css";
 import "./equipment-modal.css";
 import "./character-image-upload.css";
 import "./sheet-experience.css";
+import "./character-cosmetics.css";
 
 export default function CharactersLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return children;

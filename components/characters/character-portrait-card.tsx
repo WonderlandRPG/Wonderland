c41c14bffa8aff21d5603e2f774f668ecd8be387
WonderlandRPG <rpgwonderlandoficial@gmail.com>
2026-08-31 01:58:57 -0300

    revert: remove broken Halloween VFX rebuild

--- components/characters/character-portrait-card.tsx
+++ components/characters/character-portrait-card.tsx
@@ -50,11 +50,6 @@ export function CharacterPortraitCard({
       <RankAtmosphere rank={rank} />
 
       <div className={styles.cosmeticCardFx} aria-hidden="true">
-        <img
-          alt=""
-          className={styles.blueFlameArtwork}
-          src="/cosmetics/halloween-2026/chamas-azuis-cantos.png"
-        />
         <i className={styles.voidEclipse} />
         <i className={styles.branchVeil} />
         <i className={styles.emberField} />

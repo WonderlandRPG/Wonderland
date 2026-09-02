b22fd657839854bdc3c366de15597b9a8c811c26
WonderlandRPG <rpgwonderlandoficial@gmail.com>
2026-09-01 23:49:05 -0300

    style: make tactical reference layer authoritative

--- app/arena/mapa-tatico/layout.tsx
+++ app/arena/mapa-tatico/layout.tsx
@@ -1,7 +1,6 @@
 import type { ReactNode } from "react";
 
-import "./tactical-v2.css";
-import "./tactical-scenes.css";
+import "./tactical-reference.css";
 
 export default function TacticalMapLayout({ children }: { children: ReactNode }) {
   return <div className="tactical-route-v2">{children}</div>;

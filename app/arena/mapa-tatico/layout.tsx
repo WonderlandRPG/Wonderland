3287ef47098c3dfbb0026cb4cb864f1da386111c
WonderlandRPG <rpgwonderlandoficial@gmail.com>
2026-09-01 23:33:09 -0300

    style: load tactical scene artworks

--- app/arena/mapa-tatico/layout.tsx
+++ app/arena/mapa-tatico/layout.tsx
@@ -1,6 +1,7 @@
 import type { ReactNode } from "react";
 
 import "./tactical-v2.css";
+import "./tactical-scenes.css";
 
 export default function TacticalMapLayout({ children }: { children: ReactNode }) {
   return <div className="tactical-route-v2">{children}</div>;
